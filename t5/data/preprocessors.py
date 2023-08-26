66bc24780ef2c701d7f4113b56a405f66974376d
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-08-27 00:57:24 +0800

    Update preprocessors.py

--- t5/data/preprocessors.py
+++ t5/data/preprocessors.py
@@ -278,7 +278,7 @@ def trivia_qa_nocontext(dataset):
     A preprocessed tf.data.Dataset with the format listed above.
   """
 
-  prompt = "Question: Which American-born Sinclair won the Nobel Prize for Literature in 1930?\nAnswer: sinclair lewis\n"
+  prompt = "[S2S]Question: Which American-born Sinclair won the Nobel Prize for Literature in 1930?\nAnswer: sinclair lewis\n"
   def my_fn(x):
     """Create TriviaQA example."""
     return {
