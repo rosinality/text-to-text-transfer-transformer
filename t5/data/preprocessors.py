c1445647fa263ca88bde36045f6dffedf19a3ff5
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-08-28 14:12:06 +0800

    Update preprocessors.py

--- t5/data/preprocessors.py
+++ t5/data/preprocessors.py
@@ -354,14 +354,14 @@ def ul2_trivia_qa_nocontext_oneshot(dataset):
 def ul2_trivia_qa_nocontext_fewshot(dataset):
   def my_fn(x):
     """Create TriviaQA example."""
-    prefix = "[NLU] Question: Which American-born Sinclair won the Nobel Prize for Literature in 1930?\nAnswer: sinclair lewis.\nQuestion: "
-    surfix = "\nAnswer: <extra_id_0> \n"
-    surfix += "Question: Which branch of medicine is concerned with disorders of the blood?\nAnswer: Automated hematology.\n"
-    surfix += "Question: What are the international registration letters of a vehicle from Turkey?\nAnswer: T.R.\n"
-    surfix += "Question: Which musical featured the song Flash Bang, Wallop?\nAnswer: One-half.\n"
-    surfix += "Question: Neil Armstrong was a pilot in which war?\nAnswer: Korean.\n"
+    prefix = "[NLU] Question: Which American-born Sinclair won the Nobel Prize for Literature in 1930? Answer: sinclair lewis.\n"
+    prefix += "Question: Which branch of medicine is concerned with disorders of the blood?\nAnswer: Automated hematology.\n"
+    prefix += "Question: What are the international registration letters of a vehicle from Turkey?\nAnswer: T.R.\n"
+    prefix += "Question: Which musical featured the song Flash Bang, Wallop?\nAnswer: One-half.\n"
+    prefix += "Question: Neil Armstrong was a pilot in which war?\nAnswer: Korean.\nQuestion: "
+    suffix = "\nAnswer: <extra_id_0>"
     return {
-        'inputs': _string_join([prefix, x['question'], surfix ]),
+        'inputs': _string_join([prefix, x['question'], suffix ]),
         "targets": x["answer"]["value"],
         "answers": x["answer"]["aliases"],
     }
