1b938443af3111123c85745582921ff4355a61ba
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-08-29 14:12:23 +0800

    Update tasks.py

--- t5/data/tasks.py
+++ t5/data/tasks.py
@@ -1124,7 +1124,7 @@ TaskRegistry.add(
     "trivia_qa_v010_nocontext",
     source=seqio.TfdsDataSource(tfds_name="trivia_qa/unfiltered.nocontext:1.1.0",
                                 splits={
-                                    'validation': f'test',
+                                    'validation': f'test[:256]',
                                 }),
     preprocessors=[
         _filter_trivia_qa,
