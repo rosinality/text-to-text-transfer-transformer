700313bc0342a02f7e9aba857f5eb7d726ed7e40
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-08-31 22:14:09 +0800

    Update preprocessors.py

--- t5/data/preprocessors.py
+++ t5/data/preprocessors.py
@@ -339,7 +339,7 @@ def ul2_trivia_qa_nocontext(dataset):
     """Create TriviaQA example."""
     return {
         'inputs': _string_join_simple([
-          "[NLU] Question: ", x['question'], " Answer: <extra_id_0>" ]),
+          "[NLU] Q: ", x['question'], " \nA: <extra_id_0>" ]),
         "targets": x["answer"]["value"],
         "answers": x["answer"]["aliases"],
     }
