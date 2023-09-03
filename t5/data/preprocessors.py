7d9e74beef720e7430dc3d84932c30e063490313
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-09-03 12:26:30 +0800

    Update preprocessors.py

--- t5/data/preprocessors.py
+++ t5/data/preprocessors.py
@@ -2073,7 +2073,7 @@ def rank_classification_formatter(
 def ul2_boolq(dataset):
   return rank_classification_formatter(
     dataset,
-    inputs_format='[NLU] {passage} Question: {question}? Answer: <extra_id_0>',
+    inputs_formats='[NLU] {passage} Question: {question}? Answer: <extra_id_0>',
     targets_formats=[
       'yes',
       'no'
