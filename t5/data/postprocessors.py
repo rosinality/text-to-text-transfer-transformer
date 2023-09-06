44af90f952f6604da5071bdfee18869992d96779
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-09-06 23:16:02 +0800

    Update postprocessors.py

--- t5/data/postprocessors.py
+++ t5/data/postprocessors.py
@@ -211,17 +211,17 @@ def ul2_boolq(answer,
   del example
   if is_target:
     return answer
-  # answer
-  # answer = answer.split("<extra_id_0>")[-1].replace(".", "").replace("\n", "").lower().strip()
-  # if answer.startswith("yes"):
-  #   answer = "yes"
-  # elif answer.startswith("no"):
-  #   answer = "no"
-  # elif "yes" in answer:
-  #   answer = "yes"
-  # elif "no" in answer:
-  #   answer = "no"
-  # if (answer != "yes") and (answer != "no"):
-  #   print(f"Warning: Unrecognize: {answer}")
-  #   answer = "none"
+  answer
+  answer = answer.split("<extra_id_0>")[-1].replace(".", "").replace("\n", "").lower().strip()
+  if answer.startswith("yes"):
+    answer = "yes"
+  elif answer.startswith("no"):
+    answer = "no"
+  elif "yes" in answer:
+    answer = "yes"
+  elif "no" in answer:
+    answer = "no"
+  if (answer != "yes") and (answer != "no"):
+    print(f"Warning: Unrecognize: {answer}")
+    answer = "none"
   return answer
